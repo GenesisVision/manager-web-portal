eef9439562c6aa6830c8d5d8606e2a51d6af3ff4
nkrivous <kolsln@list.ru>
2018-03-27 11:27:27 +0300

    Update reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,19 +3,20 @@ import { loadingBarReducer } from "react-redux-loading-bar";
 import { routerReducer } from "react-router-redux";
 
 import alertMessagesReducer from "../shared/modules/alert-message/reducers/alert-message-reducers";
-import authReducer from "./authReducer";
+import authReducer from "./auth-reducer";
 import dashboardReducer from "../modules/dashboard/reducers/dashboard-reducers";
+import emailConfirmReducer from "../modules/email-confirm/reducers/email-confirm-reducers";
 import loginReducer from "../modules/login/reducers/login-reducers";
+import popupReducer from "../modules/popup/reducers/popup-reducers";
 import profileReducer from "../modules/profile/reducers/profile-reducers";
 import programReducer from "../modules/programs/reducers/program-reducers";
 import registerReducer from "../modules/register/reducers/register-reducers";
 import traderDepositReducer from "../modules/trader-deposit/reducers/trader-deposit-reducers";
-import traderReducer from "../modules/traders/reducers/trader-reducers";
+import traderReducer from "../modules/trader/reducers/trader-reducers";
 import tradersReducer from "../modules/traders/reducers/traders-reducers";
 import traderWithdrawReducer from "../modules/trader-withdraw/reducers/trader-withdraw-reducers";
-import walletAddressReducer from "../modules/wallet/reducers/wallet-address-reducer";
+import walletPaneReducer from "../modules/wallet-pane/reducers/wallet-pane-reducers";
 import walletReducer from "../modules/wallet/reducers/wallet-reducers";
-import walletTransactionsReducer from "../modules/wallet/reducers/wallet-transactions-reducer";
 
 export default combineReducers({
   routing: routerReducer,
@@ -25,13 +26,14 @@ export default combineReducers({
   loginData: loginReducer,
   registerData: registerReducer,
   authData: authReducer,
+  emailConfirmData: emailConfirmReducer,
   alertMessages: alertMessagesReducer,
   profileData: profileReducer,
   walletData: walletReducer,
-  walletTransactionsData: walletTransactionsReducer,
-  walletAddressData: walletAddressReducer,
+  walletPaneData: walletPaneReducer,
   dashboardData: dashboardReducer,
   traderDepositData: traderDepositReducer,
   traderWithdrawData: traderWithdrawReducer,
+  popupData: popupReducer,
   programFormData: programReducer
 });
