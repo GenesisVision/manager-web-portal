a04dd09416dc1c87e4065182ff67088c586a4f17
Гребенщиков Евгений <evgreb@gmail.com>
2018-05-29 20:33:27 +0300

    style fix for filters

--- src/modules/programs/components/program-filterable-list/program-filterable-list.js
+++ src/modules/programs/components/program-filterable-list/program-filterable-list.js
@@ -5,6 +5,8 @@ import ProgramListContainer from "./program-list-container/program-list-containe
 import ProgramListFilterContainer from "./program-list-filter-container/program-list-filter-container";
 import ProgramListPagingContainer from "./program-list-paging-container/program-list-paging-container";
 
+import "./program-filterable-list.css";
+
 const ProgramFilterableList = () => {
   return (
     <div className="program-filterable-list">
