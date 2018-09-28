a400d78572b22e32f6f90da1a27aae5b3bc18edf
IntensNow <d.korobskoy@mail.ru>
2018-09-28 14:53:53 +0300

    GVW-47 fix translations

--- src/pages/create-program/components/create-program-settings/create-program-settings.validators.js
+++ src/pages/create-program/components/create-program-settings/create-program-settings.validators.js
@@ -5,34 +5,37 @@ import { PROGRAM_SETTINGS_PERIOD_VALUES } from "./create-program-settings.consta
 const createProgramSettingsValidationSchema = ({ t }) =>
   Yup.object().shape({
     title: Yup.string()
-      .required(t("crete-program-page.settings.validation.title-required"))
-      .max(20, t("crete-program-page.settings.validation.title-is-long")),
+      .required(t("create-program-page.settings.validation.title-required"))
+      .max(20, t("create-program-page.settings.validation.title-is-long")),
     description: Yup.string()
       .required(
-        t("crete-program-page.settings.validation.description-required")
+        t("create-program-page.settings.validation.description-required")
       )
-      .min(20, t("crete-program-page.settings.validation.description-is-short"))
+      .min(
+        20,
+        t("create-program-page.settings.validation.description-is-short")
+      )
       .max(
         500,
-        t("crete-program-page.settings.validation.description-is-long")
+        t("create-program-page.settings.validation.description-is-long")
       ),
     currency: Yup.string().required(
-      t("crete-program-page.settings.validation.currency-required")
+      t("create-program-page.settings.validation.currency-required")
     ),
     periodLength: Yup.string()
       .oneOf(PROGRAM_SETTINGS_PERIOD_VALUES)
-      .required(t("crete-program-page.settings.validation.period-required")),
+      .required(t("create-program-page.settings.validation.period-required")),
     leverage: Yup.string().required(
-      t("crete-program-page.settings.validation.leverage-required")
+      t("create-program-page.settings.validation.leverage-required")
     ),
     entryFee: Yup.string().required(
-      t("crete-program-page.settings.validation.entry-fee-required")
+      t("create-program-page.settings.validation.entry-fee-required")
     ),
     successFee: Yup.string().required(
-      t("crete-program-page.settings.validation.success-fee-required")
+      t("create-program-page.settings.validation.success-fee-required")
     ),
     accountType: Yup.string().required(
-      t("crete-program-page.settings.validation.account-type-required")
+      t("create-program-page.settings.validation.account-type-required")
     )
   });
 
