0b2dd232bfac1c5f004655cf0a316aac6e3f97a4
IntensNow <d.korobskoy@mail.ru>
2018-10-12 16:21:03 +0300

    GVW-62 handle unauthorized users

--- src/shared/components/private-route/private-route.js
+++ src/shared/components/private-route/private-route.js
@@ -1,23 +1,28 @@
-import { Redirect, Route } from "react-router-dom";
-import React from "react";
+import { LOGIN_ROUTE } from "pages/auth/login/login.routes";
+import React, { Component } from "react";
 import { connect } from "react-redux";
+import { Redirect, Route } from "react-router-dom";
 
-const PrivateRoute = ({ component: Component, isAuthenticated, ...rest }) => {
-  return (
-    <Route
-      {...rest}
-      render={props =>
-        isAuthenticated ? (
-          <Component {...props} />
-        ) : (
-          <Redirect
-            to={{ pathname: "/login", state: { from: props.location } }}
-          />
-        )
-      }
-    />
-  );
-};
+class Private extends Component {
+  renderComponent = props => {
+    const { component: Component, isAuthenticated } = this.props;
+    return isAuthenticated ? (
+      <Component {...props} />
+    ) : (
+      <Redirect
+        to={{
+          pathname: LOGIN_ROUTE,
+          state: { from: props.location }
+        }}
+      />
+    );
+  };
+
+  render() {
+    const { component: Component, isAuthenticated, ...rest } = this.props;
+    return <Route {...rest} render={this.renderComponent} />;
+  }
+}
 
 const mapStateToProps = state => {
   const { isAuthenticated } = state.authData;
@@ -26,4 +31,5 @@ const mapStateToProps = state => {
   };
 };
 
-export default connect(mapStateToProps)(PrivateRoute);
+const PrivateRoute = connect(mapStateToProps)(Private);
+export default PrivateRoute;
