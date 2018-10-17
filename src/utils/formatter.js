d7eedc8007f51068023439798b33b172f4c96e5a
IntensNow <d.korobskoy@mail.ru>
2018-10-17 21:05:32 +0300

    GVW-73 fixes

--- src/utils/formatter.js
+++ src/utils/formatter.js
@@ -2,14 +2,11 @@ const dateFormat = date => {
   return new Date(+date).toDateString();
 };
 
-const getSign = num => (num < 0 ? "-" : "");
-
 const getDecimal = num => {
   return num - Math.floor(num);
 };
 
 const getFmnp = x => {
-  if (!x) return 0;
   let num = Number(x);
   let str = num.toString();
   const fmn = str[0];
@@ -17,23 +14,54 @@ const getFmnp = x => {
   return index + 1;
 };
 
-const removeLastZeros = arr => {
-  let result = [...arr];
+const removeLastZeros = str => {
+  let result = str.split("");
   let initialLength = result.length;
   result.reverse();
-
   for (let i = 0; i <= initialLength; i++) {
     if (Number(result[0]) !== 0) break;
 
     result = result.slice(1);
   }
   result.reverse();
-  return result;
+  return result.join("");
 };
 
 const roundingAccuracy = [null, 8, 6, 4, 2];
 
-const decreaseAccuracy = x => {
+const roundTypeEnum = {
+  ROUND: "round",
+  FLOOR: "floor",
+  CEIL: "ceil"
+};
+
+const roundLongNumber = (x, accuracy, roundType) => {
+  let result;
+  switch (roundType) {
+    case "round":
+    case "ceil":
+      result = x.toFixed(accuracy);
+      break;
+    case "floor":
+      result = x.toFixed(32).slice(0, accuracy - 32);
+      break;
+    default:
+      result = x.toFixed(accuracy);
+  }
+
+  return result;
+};
+
+const roundTypeHandler = (x, accuracy, roundType) => {
+  if (accuracy > 27) return roundLongNumber(x, accuracy, roundType);
+  let num = x * Math.pow(10, accuracy);
+  let result = Math[roundType](num) / Math.pow(10, accuracy);
+  result = result.toFixed(accuracy);
+  result = removeLastZeros(result);
+  return result;
+};
+
+const decreaseAccuracy = (x, roundType) => {
   let integer = Math.floor(x);
   integer = integer.toString();
   let charCount = integer.length;
@@ -42,10 +70,23 @@ const decreaseAccuracy = x => {
     accuracy = roundingAccuracy[charCount];
   }
 
-  return x.toFixed(accuracy);
+  let result = roundTypeHandler(x, accuracy, roundType);
+  return result;
 };
 
-const formatPositive = x => {
+const removeSign = x => {
+  if (x < 0)
+    return x
+      .split("")
+      .slice(1)
+      .join("");
+
+  return x;
+};
+
+const formatValue = (x, roundType = roundTypeEnum.FLOOR, isShowSign = true) => {
+  if (!x) return x;
+
   const decimal = getDecimal(x)
     .toFixed(32)
     .split(".")[1];
@@ -57,36 +98,21 @@ const formatPositive = x => {
     return result;
   }
 
-  if (x > 1) {
-    result = Number(decreaseAccuracy(x));
+  if (x > 1 || x < -1) {
+    result = Number(decreaseAccuracy(x, roundType));
   }
 
-  if (x < 1 && fmnp > 8) {
-    result = 1 + x;
-    result = result.toFixed(fmnp);
-    result = result.split("");
-    result[0] = "0";
-    result = result.join("");
+  if (x < 1 && x > -1 && fmnp > 8) {
+    result = roundTypeHandler(x, fmnp, roundType);
   }
 
-  if (x < 1 && fmnp <= 8) {
-    result = 1 + x;
-    result = result.toFixed(8);
-    result = result.split("");
-    result[0] = "0";
-    result = removeLastZeros(result);
-    result = result.join("");
+  if (x < 1 && x > -1 && fmnp <= 8) {
+    result = roundTypeHandler(x, 8, roundType);
   }
-  return result;
-};
 
-const formatValue = x => {
-  if (!x) return x;
-  const abs = Math.abs(x);
-  const sign = getSign(x);
-  const result = sign + formatPositive(abs);
+  if (!isShowSign) return removeSign(result);
 
   return result;
 };
 
-export { dateFormat, formatValue };
+export { dateFormat, formatValue, roundTypeEnum };
