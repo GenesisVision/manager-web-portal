7018715d5d875efb14c8245a20f6978475ab1c82
Гребенщиков Евгений <evgreb@gmail.com>
2018-10-18 16:01:02 +0300

    GVW-26 Profile: View personal detail / fix currency select

--- src/modules/currency-select/components/currency-select.js
+++ src/modules/currency-select/components/currency-select.js
@@ -1,67 +1,28 @@
 import "./currency-select.scss";
 
 import classnames from "classnames";
-import Popover from "components/popover/popover";
-import PropTypes from "prop-types";
+import Select from "components/select/select";
 import React, { Component } from "react";
 
 class CurrencySelect extends Component {
-  state = {
-    anchor: null
-  };
-
-  handleClick = event => {
-    this.setState({ anchor: event.currentTarget });
-  };
-
-  handleChildClick = value => () => {
-    if (typeof this.props.onSelect === "function") {
-      this.props.onSelect(value);
-    }
-    this.setState({ anchor: null });
-  };
-
-  handleClose = () => {
-    this.setState({ anchor: null });
-  };
-
   render() {
-    let displayValue = this.props.value;
-
-    const items = React.Children.map(this.props.children, child => {
-      const isSelected = child.props.value === this.props.value;
-      if (isSelected) displayValue = child.props.children;
-      return (
-        <div
-          className={classnames("currency__option", {
-            "currency__option--selected": isSelected
-          })}
-          onClick={this.handleChildClick(child.props.value)}
-        >
-          {child.props.children}
-        </div>
-      );
-    });
+    const { value, onChange, className, currencyValues } = this.props;
     return (
-      <div className="currency">
-        <div onClick={this.handleClick} className="currency__value">
-          {displayValue}
-        </div>
-        <Popover
-          noPadding
-          anchorEl={this.state.anchor}
-          onClose={this.handleClose}
-        >
-          <div className="currency__options">{items}</div>
-        </Popover>
-      </div>
+      <Select
+        className={classnames("currency-select", className)}
+        value={value}
+        onChange={onChange}
+      >
+        {Object.keys(currencyValues).map(currency => {
+          return (
+            <option value={currency} key={currency}>
+              {currencyValues[currency]}
+            </option>
+          );
+        })}
+      </Select>
     );
   }
 }
-
-CurrencySelect.propTypes = {
-  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
-  onSelect: PropTypes.func.isRequired
-};
 
 export default CurrencySelect;
