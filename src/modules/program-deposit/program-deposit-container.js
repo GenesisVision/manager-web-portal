3a497c8e2afd8b2a8e4a5290f3e2360352a5dce9
pavelzubov <pasha224@yandex.ru>
2018-10-26 11:51:31 +0300

    Added form error to program deposit

--- src/modules/program-deposit/program-deposit-container.js
+++ src/modules/program-deposit/program-deposit-container.js
@@ -82,7 +82,6 @@ const mapDispatchToProps = dispatch => ({
   )
 });
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(ProgramDepositContainer);
+export default connect(mapStateToProps, mapDispatchToProps)(
+  ProgramDepositContainer
+);
