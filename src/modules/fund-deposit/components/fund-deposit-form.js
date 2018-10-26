f1bffd599a3af561315261e0846d653912242e4f
pavelzubov <pasha224@yandex.ru>
2018-10-26 11:50:50 +0300

    Added form error to fund deposit

--- src/modules/fund-deposit/components/fund-deposit-form.js
+++ src/modules/fund-deposit/components/fund-deposit-form.js
@@ -10,6 +10,7 @@ import {
 } from "utils/currency-converter";
 import { formatValue } from "utils/formatter";
 import { number, object } from "yup";
+import FormError from "shared/components/form/form-error/form-error";
 
 const FundDepositForm = ({
   t,
@@ -20,7 +21,6 @@ const FundDepositForm = ({
   handleSubmit,
   errorMessage
 }) => {
-  // const fee = calculateValueOfEntryFee(values.amount, info.entryFee);
   const gvFee = calculateValueOfEntryFee(values.amount, info.gvCommission);
   const investAmount = parseFloat(values.amount || 0) - parseFloat(gvFee);
   const isAllow = values => {
@@ -81,7 +81,9 @@ const FundDepositForm = ({
           </span>
         </li>
       </ul>
-      <div className="form-error">{errorMessage}</div>
+      <div className="form-error">
+        <FormError error={errorMessage} />
+      </div>
       <div className="dialog__buttons">
         <GVButton
           type="submit"
