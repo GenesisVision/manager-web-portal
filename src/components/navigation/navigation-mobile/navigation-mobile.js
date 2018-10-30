4881360845842a71fada0b5109d0222fae437026
Гребенщиков Евгений <evgreb@gmail.com>
2018-10-30 23:00:02 +0300

    GVW-85 avatar and chip style fix

--- src/components/navigation/navigation-mobile/navigation-mobile.js
+++ src/components/navigation/navigation-mobile/navigation-mobile.js
@@ -1,8 +1,8 @@
 import { DashboardIcon } from "components/icon/dashboard-icon";
 import { DetailsIcon } from "components/icon/details-icon";
+import { FundsIcon } from "components/icon/funds-icon";
 import { LogoutIcon } from "components/icon/logout-icon";
 import { ProgramsIcon } from "components/icon/programs-icon";
-import { FundsIcon } from "components/icon/funds-icon";
 import { SearchIcon } from "components/icon/search-icon";
 import { SettingsIcon } from "components/icon/settings-icon";
 import { WalletIcon } from "components/icon/wallet-icon";
@@ -12,9 +12,9 @@ import NavigationItem, {
 import Sidebar from "components/sidebar/sidebar";
 import { LOGIN_ROUTE } from "pages/auth/login/login.routes";
 import { DASHBOARD_ROUTE } from "pages/dashboard/dashboard.routes";
+import { FUNDS_ROUTE } from "pages/funds/funds.routes";
 import { GLOBAL_SEARCH_ROUTE } from "pages/global-search/global-search.routes";
 import { PROGRAMS_ROUTE } from "pages/programs/programs.routes";
-import { FUNDS_ROUTE } from "pages/funds/funds.routes";
 import { WALLET_PAGE_ROUTE } from "pages/wallet/wallet-page";
 import PropTypes from "prop-types";
 import React from "react";
@@ -41,7 +41,7 @@ const NavigationMobile = ({
       <div className="navigation__mobile mobile">
         {isAuthenticated && (
           <div className="mobile__header">
-            <div className="mobile__avatar">
+            <div className="profile-avatar mobile__avatar">
               <img
                 alt={email}
                 className="profile-widget__image"
