095fb118f29e285dbdf7b39a11ba4ae6dcc6418f
nkrivous <kolsln@list.ru>
2018-11-07 17:38:26 +0300

    Use gvtValue on the dashboard

--- src/pages/dashboard/components/dashboard-assets/dashboard-programs/dashboard-programs.js
+++ src/pages/dashboard/components/dashboard-assets/dashboard-programs/dashboard-programs.js
@@ -107,7 +107,7 @@ class DashboardPrograms extends Component {
               <ProgramPeriodEnd periodEnds={program.periodEnds} />
             </TableCell>
             <TableCell className="programs-table__cell dashboard-programs__cell--value">
-              {formatPercent(program.personalDetails.value)} GVT
+              {formatPercent(program.personalDetails.gvtValue)} GVT
             </TableCell>
             <TableCell className="programs-table__cell dashboard-programs__cell--profit">
               <NumberFormat
